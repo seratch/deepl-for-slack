ad17910a0e999e6915017687b18c079900c61fa5
Kazuhiro Sera <seratch@gmail.com>
2021-09-13 15:30:14 +0900

    Fix #5 type error

--- src/runnner.ts
+++ src/runnner.ts
@@ -1,10 +1,10 @@
-import { View } from '@slack/types';
+import { PlainTextOption, View } from '@slack/types';
 import { WebClient } from '@slack/web-api';
 import { Option } from '@slack/types';
 import { langToReaction, langToName } from './languages';
 
 const orderedLangNames = (process.env.DEEPL_RUNNER_LANGUAGES || "en,ja,zh,de,fr,it,es,nl,pl,pt,ru").split(",");
-const lanaguageOptions: Option[] = orderedLangNames.filter(l => langToReaction[l]).map(lang => {
+const lanaguageOptions: PlainTextOption[] = orderedLangNames.filter(l => langToReaction[l]).map(lang => {
   return {
     "text": {
       "type": "plain_text",
