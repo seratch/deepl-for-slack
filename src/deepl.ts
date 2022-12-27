08d64f2a9f7f4ee88e13c3db31c2c5782a76dc2f
Kazuhiro Sera <seratch@gmail.com>
2022-12-27 09:26:43 +0900

    Format code

--- src/deepl.ts
+++ src/deepl.ts
@@ -10,107 +10,119 @@ export class DeepLApi {
     this.authKey = authKey;
     this.logger = logger;
     const usingFreePlan = process.env.DEEPL_FREE_API_PLAN === "1";
-    const apiSubdomain = usingFreePlan ? 'api-free' : 'api';
+    const apiSubdomain = usingFreePlan ? "api-free" : "api";
     this.axiosInstance = axios.create({
       baseURL: `https://${apiSubdomain}.deepl.com/v2`,
       timeout: 30000,
     });
   }
 
-  async translate(text: string, targetLanguage: string): Promise<string | null> {
+  async translate(
+    text: string,
+    targetLanguage: string
+  ): Promise<string | null> {
     return this.axiosInstance({
       url: "/translate",
       data: qs.stringify({
         auth_key: this.authKey,
-
-        // match special parsing
-        text: text.replace(/<(.*?)>/g, function(i: any, match: string) {
-
-          // match #channels and @mentions
-          if(match.match(/^[#@].*$/)) {
-            const matched = match.match(/^([#@].*)$/);
-            if (matched != null) {
-              return '<mrkdwn>' + matched[1] + '</mrkdwn>';
-            }
-            return '';
-          }
-
-          // match subteam
-          if(match.match(/^!subteam.*$/)) {
-            return '@[subteam mention removed]';
-          }
-
-          // match date formatting
-          if(match.match(/^!date.*$/)) {
-            const matched = match.match(/^(!date.*)$/);
-            if (matched != null) {
-              return '<mrkdwn>' + matched[1] + '</mrkdwn>';
-            }
-            return '';
-          }
-
-          // match special mention
-          if(match.match(/^!.*$/)) {
-            const matched = match.match(/^!(.*?)(?:\|.*)?$/);
-            if (matched != null) {
-              return '<ignore>@' + matched[1] + '</ignore>';
-            }
-            return '<ignore>@[special mention]</ignore>';
-          }
-
-          // match formatted link
-          if(match.match(/^.*?\|.*$/)) {
-            const matched = match.match(/^(.*?)\|(.*)$/);
-            if (matched != null) {
-              return '<a href="' + matched[1] + '">' + matched[2] + '</a>';
-            }
-            return '';
-          }
-
-          // fallback (raw link or unforeseen formatting)
-          return '<mrkdwn>' + match + '</mrkdwn>';
-
-        // match emoji
-        }).replace(/:([a-z0-9_-]+):/g, function(i: any, match: string) {
-          return '<emoji>' + match + '</emoji>';
-        }),
+        text:
+          // Before sending the text to the DeepL API,
+          // replace special syntax parts with ignore tags to keep them
+          text
+            .replace(/<(.*?)>/g, (_: any, match: string) => {
+              // match #channels and @mentions
+              if (match.match(/^[#@].*$/)) {
+                const matched = match.match(/^([#@].*)$/);
+                if (matched != null) {
+                  return "<mrkdwn>" + matched[1] + "</mrkdwn>";
+                }
+                return "";
+              }
+              // match subteam
+              if (match.match(/^!subteam.*$/)) {
+                return "@[subteam mention removed]";
+              }
+              // match date formatting
+              if (match.match(/^!date.*$/)) {
+                const matched = match.match(/^(!date.*)$/);
+                if (matched != null) {
+                  return "<mrkdwn>" + matched[1] + "</mrkdwn>";
+                }
+                return "";
+              }
+              // match special mention
+              if (match.match(/^!.*$/)) {
+                const matched = match.match(/^!(.*?)(?:\|.*)?$/);
+                if (matched != null) {
+                  return "<ignore>@" + matched[1] + "</ignore>";
+                }
+                return "<ignore>@[special mention]</ignore>";
+              }
+              // match formatted link
+              if (match.match(/^.*?\|.*$/)) {
+                const matched = match.match(/^(.*?)\|(.*)$/);
+                if (matched != null) {
+                  return '<a href="' + matched[1] + '">' + matched[2] + "</a>";
+                }
+                return "";
+              }
+              // fallback (raw link or unforeseen formatting)
+              return "<mrkdwn>" + match + "</mrkdwn>";
+              // match emoji
+            })
+            .replace(/:([a-z0-9_-]+):/g, (_, match: string) => {
+              return "<emoji>" + match + "</emoji>";
+            }),
         target_lang: targetLanguage.toUpperCase(),
-        tag_handling: 'xml',
-        ignore_tags: 'emoji,mrkdwn,ignore'
+        tag_handling: "xml",
+        ignore_tags: "emoji,mrkdwn,ignore",
       }),
       headers: {
-        "content-type": "application/x-www-form-urlencoded;charset=utf-8"
-      }
-    }).then(response => {
-      this.logger.debug(response.data);
-      if (response.data.translations && response.data.translations.length > 0) {
-
-        // match emoji
-        return response.data.translations[0].text.replace(/<emoji>([a-z0-9_-]+)<\/emoji>/g, function(i: any, match: string) {
-          return ':' + match + ':';
-
-        // match <mrkdwn>...</mrkdwn>
-        }).replace(/<mrkdwn>(.*?)<\/mrkdwn>/g, function(i: any, match: string) {
-          return '<' + match + '>';
-
-        // match <a href="...">...</a>
-        }).replace(/(<a href="(?:.*?)">(?:.*?)<\/a>)/g, function(i: any, match: string) {
-          const matched = match.match(/<a href="(.*?)">(.*?)<\/a>/);
-          if (matched != null) {
-            return '<' + matched[1] + '|' + matched[2] + '>';
-          }
-          return '';
-
-        // match <ignore>...</ignore>
-        }).replace(/<ignore>(.*?)<\/ignore>/g, function(i: any, match: string) {
-          return match;
-        });
-      } else {
-        return ":x: Failed to translate it due to an unexpected response from DeepL API";
-      }
-    }).catch(error => {
-      this.logger.error(`Failed to translate - text: ${text} error: ${error}`);
-      return `:x: Failed to translate it due to ${error}`;
-    });
+        "content-type": "application/x-www-form-urlencoded;charset=utf-8",
+      },
+    })
+      .then((response) => {
+        this.logger.debug(response.data);
+        if (
+          response.data.translations &&
+          response.data.translations.length > 0
+        ) {
+          // Parse ecoding tags to restore the original special syntax
+          return response.data.translations[0].text
+            .replace(
+              /<emoji>([a-z0-9_-]+)<\/emoji>/g,
+              (_: any, match: string) => {
+                return ":" + match + ":";
+                // match <mrkdwn>...</mrkdwn>
+              }
+            )
+            .replace(/<mrkdwn>(.*?)<\/mrkdwn>/g, (_: any, match: string) => {
+              return "<" + match + ">";
+              // match <a href="...">...</a>
+            })
+            .replace(
+              /(<a href="(?:.*?)">(?:.*?)<\/a>)/g,
+              (_: any, match: string) => {
+                const matched = match.match(/<a href="(.*?)">(.*?)<\/a>/);
+                if (matched != null) {
+                  return "<" + matched[1] + "|" + matched[2] + ">";
+                }
+                return "";
+                // match <ignore>...</ignore>
+              }
+            )
+            .replace(/<ignore>(.*?)<\/ignore>/g, (_: any, match: string) => {
+              return match;
+            });
+        } else {
+          return ":x: Failed to translate it due to an unexpected response from DeepL API";
+        }
+      })
+      .catch((error) => {
+        this.logger.error(
+          `Failed to translate - text: ${text} error: ${error}`
+        );
+        return `:x: Failed to translate it due to ${error}`;
+      });
   }
 }
--- src/runnner.ts
+++ src/runnner.ts
@@ -1,154 +1,163 @@
-import { PlainTextOption, View } from '@slack/types';
-import { WebClient } from '@slack/web-api';
-import { langToReaction, langToName } from './languages';
+import { PlainTextOption, View } from "@slack/types";
+import { WebClient } from "@slack/web-api";
+import { langToReaction, langToName } from "./languages";
 
-const orderedLangNames = (process.env.DEEPL_RUNNER_LANGUAGES || "en,ja,zh,de,fr,it,es,nl,pl,pt,ru,bg,cs,da,el,et,fi,hu,id,lt,ro,sk,sl,sv,tr,uk").split(",");
-const lanaguageOptions: PlainTextOption[] = orderedLangNames.filter(l => langToReaction[l]).map(lang => {
-  return {
-    "text": {
-      "type": "plain_text",
-      "text": `${langToReaction[lang]} ${langToName[lang]}`
-    },
-    "value": lang
-  };
-});
+const orderedLangNames = (
+  process.env.DEEPL_RUNNER_LANGUAGES ||
+  "en,ja,zh,de,fr,it,es,nl,pl,pt,ru,bg,cs,da,el,et,fi,hu,id,lt,ro,sk,sl,sv,tr,uk"
+).split(",");
+const lanaguageOptions: PlainTextOption[] = orderedLangNames
+  .filter((l) => langToReaction[l])
+  .map((lang) => {
+    return {
+      text: {
+        type: "plain_text",
+        text: `${langToReaction[lang]} ${langToName[lang]}`,
+      },
+      value: lang,
+    };
+  });
 
 export async function openModal(client: WebClient, triggerId: string) {
   await client.views.open({
     trigger_id: triggerId,
-    view: buildNewModal(orderedLangNames[0])
+    view: buildNewModal(orderedLangNames[0]),
   });
 }
 
 export function buildNewModal(lang: string): View {
   return {
-    "type": "modal",
-    "callback_id": "run-translation",
-    "title": {
-      "type": "plain_text",
-      "text": "DeepL API Runner :books:"
+    type: "modal",
+    callback_id: "run-translation",
+    title: {
+      type: "plain_text",
+      text: "DeepL API Runner :books:",
     },
-    "submit": {
-      "type": "plain_text",
-      "text": "Translate"
+    submit: {
+      type: "plain_text",
+      text: "Translate",
     },
-    "blocks": [
+    blocks: [
       {
-        "type": "input",
-        "block_id": "text",
-        "element": {
-          "type": "plain_text_input",
-          "action_id": "a",
-          "multiline": true,
-          "placeholder": {
-            "type": "plain_text",
-            "text": "Put the text to translate"
-          }
+        type: "input",
+        block_id: "text",
+        element: {
+          type: "plain_text_input",
+          action_id: "a",
+          multiline: true,
+          placeholder: {
+            type: "plain_text",
+            text: "Put the text to translate",
+          },
         },
-        "label": {
-          "type": "plain_text",
-          "text": "Text"
-        }
+        label: {
+          type: "plain_text",
+          text: "Text",
+        },
       },
       {
-        "type": "input",
-        "block_id": "lang",
-        "element": {
-          "type": "static_select",
-          "action_id": "a",
-          "placeholder": {
-            "type": "plain_text",
-            "text": "Choose language"
+        type: "input",
+        block_id: "lang",
+        element: {
+          type: "static_select",
+          action_id: "a",
+          placeholder: {
+            type: "plain_text",
+            text: "Choose language",
           },
-          "initial_option": {
-            "text": {
-              "type": "plain_text",
-              "text": `${langToReaction[lang]} ${langToName[lang]}`
+          initial_option: {
+            text: {
+              type: "plain_text",
+              text: `${langToReaction[lang]} ${langToName[lang]}`,
             },
-            "value": lang
+            value: lang,
           },
-          "options": lanaguageOptions
+          options: lanaguageOptions,
         },
-        "label": {
-          "type": "plain_text",
-          "text": "Language",
-          "emoji": true
-        }
-      }
-    ]
+        label: {
+          type: "plain_text",
+          text: "Language",
+          emoji: true,
+        },
+      },
+    ],
   };
 }
 
 export function buildLoadingView(lang: string, text: string): View {
   return {
-    "type": "modal",
-    "title": {
-      "type": "plain_text",
-      "text": "DeepL API Runner :books:"
+    type: "modal",
+    title: {
+      type: "plain_text",
+      text: "DeepL API Runner :books:",
     },
-    "blocks": [
+    blocks: [
       {
-        "type": "section",
-        "text": {
-          "type": "plain_text",
-          "text": `Translating the text into ${langToReaction[lang]} ...`
-        }
+        type: "section",
+        text: {
+          type: "plain_text",
+          text: `Translating the text into ${langToReaction[lang]} ...`,
+        },
       },
       {
-        "type": "divider"
+        type: "divider",
       },
       {
-        "type": "section",
-        "text": {
-          "type": "plain_text",
-          "text": text
-        }
-      }
-    ]
+        type: "section",
+        text: {
+          type: "plain_text",
+          text: text,
+        },
+      },
+    ],
   };
 }
 
-export function buildResultView(lang: string, sourceText: string, translatedText: string): View {
+export function buildResultView(
+  lang: string,
+  sourceText: string,
+  translatedText: string
+): View {
   return {
-    "type": "modal",
-    "callback_id": "new-runner",
-    "title": {
-      "type": "plain_text",
-      "text": "DeepL API Runner :books:"
+    type: "modal",
+    callback_id: "new-runner",
+    title: {
+      type: "plain_text",
+      text: "DeepL API Runner :books:",
     },
-    "submit": {
-      "type": "plain_text",
-      "text": "Try Another"
+    submit: {
+      type: "plain_text",
+      text: "Try Another",
     },
-    "private_metadata": lang,
-    "blocks": [
+    private_metadata: lang,
+    blocks: [
       {
-        "type": "section",
-        "text": {
-          "type": "plain_text",
-          "text": `Here is the same text in ${langToReaction[lang]}`
-        }
+        type: "section",
+        text: {
+          type: "plain_text",
+          text: `Here is the same text in ${langToReaction[lang]}`,
+        },
       },
       {
-        "type": "divider"
+        type: "divider",
       },
       {
-        "type": "section",
-        "text": {
-          "type": "plain_text",
-          "text": translatedText
-        }
+        type: "section",
+        text: {
+          type: "plain_text",
+          text: translatedText,
+        },
       },
       {
-        "type": "divider"
+        type: "divider",
       },
       {
-        "type": "section",
-        "text": {
-          "type": "plain_text",
-          "text": sourceText
-        }
-      }
-    ]
-  }
+        type: "section",
+        text: {
+          type: "plain_text",
+          text: sourceText,
+        },
+      },
+    ],
+  };
 }
